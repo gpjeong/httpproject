9b25cf457e2b44fb9b96c9eb2a5869f87d91e149
정광필 <kp.jeong@okestro.com>
2023-11-02 09:54:52 +0900

    delete server 로직 수정

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"httpproject/internal/config"
 	"httpproject/internal/datastore"
 	"httpproject/util/logger"
 	"net/http"
@@ -15,13 +16,13 @@ func ApiTest() {
 	// GET 요청
 	r.GET("/get", func(c *gin.Context) {
 
-		inputParam := c.Query("name")
+		nameParam := c.Query("name")
 
-		if inputParam == "" {
+		if nameParam == "" {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "input 파라미터가 필요합니다"})
 		} else {
 			query := make(map[string]interface{}, 0)
-			query["name = ?"] = inputParam
+			query["name = ?"] = nameParam
 
 			dataCount, err := datastore.DBService().GetCountCustomQuery(tableStruct, query)
 			if err != nil {
@@ -30,11 +31,11 @@ func ApiTest() {
 
 			if dataCount != 0 {
 				c.JSON(200, gin.H{
-					"message": fmt.Sprintf("%s 조회에 성공하였습니다", inputParam),
+					"message": fmt.Sprintf("%s 조회에 성공하였습니다", nameParam),
 				})
 			} else {
 				c.JSON(500, gin.H{
-					"message": fmt.Sprintf("%s 조회에 실패하였습니다", inputParam),
+					"message": fmt.Sprintf("%s 조회에 실패하였습니다", nameParam),
 				})
 			}
 		}
@@ -51,10 +52,43 @@ func ApiTest() {
 
 	// DELETE 요청
 	r.DELETE("/delete", func(c *gin.Context) {
-		id := c.Param("id")
-		c.JSON(http.StatusOK, gin.H{
-			"message": fmt.Sprintf("DELETE request for resource with ID %s", id),
-		})
+		idParam := c.DefaultQuery("id", "")
+		nameParam := c.DefaultQuery("name", "")
+
+		if nameParam == "" || idParam == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "input 파라미터가 필요합니다"})
+		} else {
+			dataList := make([]OjtInfo, 0)
+			data := OjtInfo{
+				Id:   idParam,
+				Name: nameParam,
+			}
+			dataList = append(dataList, data)
+
+			query := make(map[string]interface{}, 0)
+			query["name = ?"] = nameParam
+			query["id = ?"] = idParam
+
+			dataCount, err := datastore.DBService().GetCountCustomQuery(tableStruct, query)
+			if err != nil {
+				logger.Log.Error().Msgf("DB Get Name error :", err.Error())
+			}
+
+			if dataCount != 0 {
+				_, err := datastore.DBService().DeleteCustomQuery(query, data)
+				if err != nil {
+					logger.Log.Error().Msgf("Delete Data Failed :", err.Error())
+				}
+				c.JSON(200, gin.H{
+					"message": fmt.Sprintf("Id : %s, name : %s 삭제 성공하였습니다", idParam, nameParam),
+				})
+			} else {
+				c.JSON(500, gin.H{
+					"message": fmt.Sprintf("Id : %s, name : %s 삭제 실패하였습니다", idParam, nameParam),
+				})
+			}
+		}
+
 	})
 
 	// PATCH 요청
@@ -106,5 +140,5 @@ func ApiTest() {
 	})
 
 	// 서버 시작
-	r.Run(":8080")
+	r.Run(":" + config.ServerConfig.ApiInfo.ApiPort)
 }
